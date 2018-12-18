17a52af19db4b95125fd5a6d5b33b2bf76586d19
pyfaith <pyfaith@foxmail.com>
2018-12-18 19:53:33 +0800

    实现文件断点续传功能

--- FTP_client/bin/ftp_client.py
+++ FTP_client/bin/ftp_client.py
@@ -7,7 +7,7 @@
 import optparse
 import socket
 import json
-
+import os
 
 class ClientHandler(object):
 
@@ -24,6 +24,7 @@ class ClientHandler(object):
         self.verify_args(self.options, self.args)
 
         self.make_connection()
+        self.main_path = os.path.dirname(os.path.abspath(__file__))
 
     def verify_args(self, options, agrs):
         '''验证ip和port'''
@@ -47,8 +48,99 @@ class ClientHandler(object):
 
     def interactive(self):
         '''开始用户交互'''
+        print("begin to interactive ......")
         if self.authenticate():
-            pass
+
+            cmd_info = input("[{user}]".format(user=self.user)).strip() #eg put test.py testdir
+
+            cmd_list = cmd_info.split()
+            if hasattr(self, cmd_list[0]):
+                func = getattr(self, cmd_list[0])
+                func(*cmd_list)
+
+    def put(self, *cmd_list):
+        '''put af.png dirimages'''
+        action, local_path, target_path = cmd_list
+        local_path = os.path.join(self.main_path, local_path)
+        file_name = os.path.basename(local_path)
+        file_size = os.stat(local_path).st_size
+        data = {
+            "action": "put",
+            "file_name": file_name,
+            "file_size": file_size,
+            "target_path": target_path,
+        }
+        self.sock.send(json.dumps(data).encode("utf8"))
+
+        #接受服务端返回信息
+        is_exists = self.sock.recv(1024).decode("utf8")
+        # print(is_exists,"*********************")
+
+        ###################****************###########################
+        #文件上传
+        '''
+        client：
+            用户输入：put test.mp4 testdir
+                c1. 发送文件相关数句至server
+                c2. server回应是否已有文件，有client询问用户是否续写：
+                        是：回应server Y
+                            续传文件
+                        否： 回应server N
+                            重新上传文件
+
+
+
+        server
+                s1. 获取数据 data={'action':'put', 'filename': 'test.mp4'...}
+                    判断本地是否有该文件存在：
+                        是：
+                            文件完整：   
+                                s2. 回应client以有完整文件
+                            文件不完整：  
+                                s2. 回应client文件不完整，是否续传，等待client回应：
+                                        Y：续写文件
+                                        N： 重新文件
+
+                        否： 
+                            s2.回应客户端server没有该文件
+                            直接写文件
+
+
+        '''
+        has_sent=0
+        if is_exists == "800": #文件不完整******
+            ''''''
+            choice = input("this file exist, but not enough, is conutine? [[Y/N or y/n]]").strip()
+            if choice.upper() == "Y":
+                self.sock.sendall("Y".encode("utf8"))
+
+                continue_position = self.sock.recv(1024).decode("utf8") #获取服务端已有文件大小
+
+
+
+            else:
+                self.sock.sendall("N".encode("utf8"))
+
+        elif is_exists == "801": #服务端文件存在，并且完整
+            return
+
+
+        f = open(local_path, "rb")
+        while has_sent < file_size:
+            data = f.read(1024)
+            self.sock.sendall(data)
+            has_sent += len(data)
+
+        f.close()
+        print("put success!")
+        ###################****************###########################
+
+
+
+
+
+
+
 
     def authenticate(self):
         '''检查用户输入账号'''
@@ -73,7 +165,14 @@ class ClientHandler(object):
         }
         data = self.sock.send(json.dumps(data).encode("utf8"))
         response = self.response()
-        print(response)
+
+        if response["status_code"] == 254:
+            self.user = user
+            print(response["status_msg"])
+            return True
+        else:
+            print(response)
+
 
 
 
--- FTP_server/core/server.py
+++ FTP_server/core/server.py
@@ -7,27 +7,32 @@
 import socketserver
 import json
 import configparser
+import os
 
-STATUS_CODE = {
-    250: "",
-    251: "",
-    252: "",
-    253: "",
-    254: "",
-    255: "",
-    256: "",
-    250: "",
-    250: "",
-    250: "",
-    250: "",
-    250: "",
-    250: "",
-    250: "",
-    250: "",
-}
+from conf import settings
+
 
 
 from conf import settings
+
+
+STATUS_CODE = {
+    250: """Invaild cmd format #eg: {"action": "get", "filename": "test.py", "size": "1234"}""",
+    251: "Invaild cmd",
+    252: "Invaild auth data",
+    253: "Wrong username or password",
+    254: "Passed authentication",
+    255: "Filename doesn`t provided",
+    256: "File doesn`t exist on server",
+    257: "Ready to send file",
+    258: "md5 verification",
+
+    800: "the file exist, but not enough, is contnue?",
+    801: "the file exist!",
+    802: "Ready to receive datas",
+
+    900: "md5 valdate success",
+}
 
 class ServerHandler(socketserver.BaseRequestHandler):
 
@@ -54,6 +59,8 @@ class ServerHandler(socketserver.BaseRequestHandler):
             else:#error
                 pass
 
+
+
     def send_response(self, status_code):
         response = {"status_code": status_code, "status_msg": STATUS_CODE[status_code],}
         self.request.sendall(json.dumps(response).encode("utf8"))
@@ -78,8 +85,88 @@ class ServerHandler(socketserver.BaseRequestHandler):
 
             if pwd == cfg[user]["Password"]:
                 self.user = user
-                print("用户验证成功")
+                self.main_path = os.path.join(settings.BASE_DIR, "home", user)
+                print("用户验证成功:", user)
                 return user
+
+
+    def put(self, **data):
+        '''接受客户端数据
+        put 13.png iamges
+        '''
+        # print("put:", data)
+        file_name = data.get("file_name")
+        file_size = data.get("file_size")
+        target_path = data.get("target_path")
+
+        abs_path = os.path.join(self.main_path, target_path, file_name) #eg /home/faith/66.png
+
+        ###################****************###########################
+        #client 文件上传分析:
+        '''
+        client：
+            用户输入：put test.mp4 testdir
+                c1. 发送文件相关数句至server
+                c2. server回应是否已有文件，有client询问用户是否续写：
+                        是：回应server Y
+                            续传文件
+                        否： 回应server N
+                            重新上传文件
+                        
+        
+        
+        server
+                s1. 获取数据 data={'action':'put', 'filename': 'test.mp4'...}
+                    判断本地是否有该文件存在：
+                        是：
+                            文件完整：   
+                                s2. 回应client以有完整文件
+                            文件不完整：  
+                                s2. 回应client文件不完整，是否续传，等待client回应：
+                                        Y：续写文件
+                                        N： 重新文件
+                                              
+                        否： 
+                            s2.回应客户端server没有该文件
+                            直接写文件
+        
+        
+        '''
+        has_received = 0
+        if os.path.exists(abs_path):
+            file_has_size = os.stat(abs_path).st_size #获取本地存放文件的大小
+            if file_has_size < file_size:#断点续传
+                self.request.sendall("800".encode("utf8"))
+                choice = self.request.recv(1024).decode("utf8")
+                if choice == "Y": #用户需要续传文件
+                    self.request.sendall(str(file_has_size).encode("utf8")) #告诉客户端本地已有文件的数据大小
+
+                    f = open(abs_path, "ab")
+                    has_received += file_has_size #重置 文件写 循环条件
+
+                else: #用户不续传，重新复写文件
+                    f = open(abs_path, "wb")
+
+
+            else: #服务端文件是完整文件
+                self.request.sendall("801".encode("utf8"))
+                return
+
+        else: #服务端没有该文件
+            self.request.sendall("802".encode("utf8"))
+
+            f = open(abs_path, "wb")
+
+        #接受客户端发送过来的文件
+        while has_received < file_size:
+            data = self.request.recv(1024)
+            f.write(data)
+            has_received += len(data)
+        f.close()
+
+        ###################****************###########################
+
+
 
 
 
